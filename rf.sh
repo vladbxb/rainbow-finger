6ae9bab8f043e00d6ff0569b71ba6c340a05b5c6
vladbxb <135911347+vladbxb@users.noreply.github.com>
2025-02-26 23:31:12 +0200

    Update and rename o_da.sh to rf.sh

--- rf.sh
+++ rf.sh
@@ -20,14 +20,3 @@ else
         sleep 0.05
     done
 fi
-
-# list all colors
-#
-#
-# for i in {1..7}
-# do
-#      FINGER_OUTPUT=$(finger debian)
-#      RANDOM_NUMBER=$((RANDOM % 7 + 1)) 
-#      echo -e "\e[1;3${RANDOM_NUMBER}m $FINGER_OUTPUT \e[0m" 
-#      sleep 0.1  
-# done
